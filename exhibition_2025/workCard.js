daac56ba916babd6db1f8115e96b593cc88b7fce
AkiYaSshi <9tu4or5@gmail.com>
2025-11-05 01:22:30 +0800

    作品頁排版&logo

---
+++ exhibition_2025/workCard.js
@@ -0,0 +1,8 @@
+const card = document.createElement("div");
+card.className = "card";
+card.innerHTML = `
+                    <a href="#aa-00" class="description" title="作品資訊">
+                        <h2>Under Spotlight</h2>
+                        <img id="aa-00" src="img/04/4-2-01.png" frameborder="0" scrolling="no" allowfullscreen=allowfullscreen />
+                    </a>
+                `;
