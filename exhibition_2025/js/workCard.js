e3156ffe33dc8ffea623317bfcbc4f5a5a7d6c70
AkiYaSshi <9tu4or5@gmail.com>
2025-11-05 02:50:13 +0800

    更改生成作品卡結構

--- exhibition_2025/js/workCard.js
+++ exhibition_2025/js/workCard.js
@@ -1,10 +1,17 @@
+
 let videos = [], games = [], medias = [];
 let nowContainer;
+const body = document.querySelector('body');
+
+createBase("aa");
+createBase("md");
+createBase("nma");
 
 const videoContainer = document.getElementById("aa-container");
 const gameContainer = document.getElementById("md-container");
 const mediaContainer = document.getElementById("nma-container");
 
+nowContainer = videoContainer;
 fetch('https://raw.githubusercontent.com/AkiYaSshi/Exhibition_2025.github.io/master/exhibition_2025/works.json')
     .then(response => {
         if (!response.ok) {
@@ -17,14 +24,43 @@ fetch('https://raw.githubusercontent.com/AkiYaSshi/Exhibition_2025.github.io/mas
         games = data.games;
         medias = data.medias;
         createWorks(videos, videoContainer, "aa");
-        createWorks(games, gameContainer, "mb");
+        createWorks(games, gameContainer, "md");
         createWorks(medias, mediaContainer, "nma");
     })
     .catch (error => {
         console.error("載入 JSON 失敗：", error);
-        title.textContent = "載入失敗";
         nowContainer.innerHTML = "<p style='color:grey;'>無法載入作品資料</br>請聯繫網站程式</p>";
     });
+function createBase(sec) {
+    const section = document.createElement("div");
+    section.className = "work-section";
+    section.id = sec;
+    section.innerHTML = `
+                            <div class="frame">
+                                <div class="title">
+                                    <a href="#aa">
+                                        <span class="title-card">
+                                            <img class="background" src="img/04/4-2-01.png" />
+                                        </span>
+                                    </a>
+                                    <a href="#md">
+                                        <span class="title-card">
+                                            <img class="background" src="img/04/4-2-02.png" />
+                                        </span>
+                                    </a>
+                                    <a href="#nma">
+                                        <span class="title-card">
+                                            <img class="background" src="img/04/4-2-03.png" />
+                                        </span>
+                                    </a>
+                                </div>
+                                <div class="works-container" id="${sec}-container">
+                                </div>
+                            </div>
+                            <div class="noise-texture"></div>
+                        `;
+    body.appendChild(section);
+}
 function createWorks(type, container, name) {
     type.forEach((work, index) => {
         const card = document.createElement("div");
@@ -36,6 +72,6 @@ function createWorks(type, container, name) {
                             </a>
                         `;
 
+        container.appendChild(card);
     });
-    container.appendChild(card);
 }
